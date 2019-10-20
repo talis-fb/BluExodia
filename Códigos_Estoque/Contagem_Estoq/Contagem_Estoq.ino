7cc12b91824e510c63d6de6cd7e5f0d6373f74eb
GuaxinimRadical <54823205+GuaxinimRadical@users.noreply.github.com>
2019-10-20 13:14:29 -0300

    Correção Completa do Código de Contagem de Estoque

--- Códigos_Estoque/Contagem_Estoq/Contagem_Estoq.ino
+++ Códigos_Estoque/Contagem_Estoq/Contagem_Estoq.ino
@@ -1,16 +1,22 @@
+#include <MFRC522.h>
 #include <BluExodia.h>
 
 BluExodia guy;
 
+#define quantEs 5 //<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Quantidade de produtos que serão registrados
+
 int start;
-int quantEs;
 int ProdutoLidos;
+
+int cont[quantEs];//Vetor que guarda a quantidade lida de cada produto
+String estoqProd[quantEs]; //Vetor que guarda o nome dos produtos lidos
+String estoqValor[quantEs];//Vetor que guarda o valor dos produtos lidos
 
 void setup() {
   guy.Hearth();
 
   Serial.println("Contagem de Estoque");
-  Serial.print("Insira quantos produtos há para registrar no estoque no Serial: ");
+  Serial.print((String)quantEs+" Produtos para serem contados... \n");
   ProdutoLidos = 0;
   start=0;
 }
@@ -18,68 +24,75 @@ void setup() {
 
 void loop() {
   bool newProd = false; //Variavel para saber se o 
-  
-  if (Serial.available() > 0 && start<1) {
-    quantEs = Serial.read();
-    start=1;
-    Serial.print("Insira os produtos...");
-    Serial.println("(Digite 2 no Serial caso queira saber sobre a contagem)\n");
-  }
-  
-  if(start>0){
-    String estoqProd[quantEs]; //Vetor que guarda o nome dos produtos lidos
-    String estoqValor[quantEs];//Vetor que guarda o valor dos produtos lidos
-    int cont[quantEs];         //Vetor que guarda a quantidade lida de cada produto
+ 
+    
      /*No caso, o vetor 1 de cont[] guarda a quantidade de produtos lidos do vetor 1 de EstoqProd[]
       *Cada produto novo se registrará como um novo vetor no EstoqProd[] 
       *e caso outro igual passe, apenas cont[] se somará mais 1.*/
 
       //Bloco que ler o cartão no leitor e guarda a leitura dos blocos em variaveis temporariais
         guy.In();
-        String tProd = guy.ReadToStr(62);
-        String tVal = guy.ReadToStr(61);
+        
+        guy.Readf(62);
+        String tProd = guy.Namef();
+        guy.Readf(61);
+        String tVal = guy.Namef();
+        
         guy.end();
+        
       //----------------------
+
       
       //Bloco que verifica se caso o produto no leitor já tenha sido registrado em estoqProd[]
+      if(tProd!="" && tVal!=""){
+        int verif = 0;
        for(int c=0;c<quantEs;c++){
-          if(tProd == estoqProd[c]){
+          if(tProd.equals(estoqProd[c]) ){
             cont[c]++;
-            Serial.println("Produto: "+estoqProd[c]+"+1");
-          } else if(c==(quantEs-1)){
+            Serial.println((String)"Produto: "+estoqProd[c]+"+1");
+            verif=1;
+          }
+          if(c==(quantEs-1) && verif==0 ){
               newProd=true;
-              Serial.println("Produto NOVO: ");
+              Serial.print("Produto NOVO: ");
+              verif=0;
             }
+          }
+       }else{
+            Serial.println("Erro, cartão inválido...\n");
+            return;
        }
        //----------------------
 
        //Caso o produto não tenha sido registrado, registrasse no proximo vetor vazio
-         if(newProd){
-          estoqProd[ProdutoLidos]  = tProd;
-          estoqValor[ProdutoLidos] = tVal;
-            ProdutoLidos++;
-          Serial.print("Adicionado :)");
-          }
+         if(newProd==true){
+          if(ProdutoLidos<quantEs){
+            estoqProd[ProdutoLidos]  = tProd;
+            estoqValor[ProdutoLidos] = tVal;
+            cont[ProdutoLidos] = 1;
+              ProdutoLidos++;
+            Serial.print("Adicionado :)\n");
+            newProd=false;
+          }else{
+            Serial.print("Limite de Produtos Registrados :(\n");
+            newProd=false;
+            }
+         }
         //----------------------     
 
-    //Bloco para mostrar as informações da contagem caso seja solicitado
-    if (Serial.available() > 0) {
-     int deku = Serial.read();
-      if(deku=='2'){
-      Serial.println("------------------------");
-      Serial.println("Expectativa de Produtos: "+quantEs);
-      Serial.println("Produtos já contados:    "+ProdutoLidos);
+    //Bloco para mostrar as informações da contagem
+      Serial.println("\n------------------------");
+      Serial.println((String)"Expectativa de Produtos: "+quantEs);
+      Serial.println((String)"Produtos já contados:    "+ProdutoLidos);
       if(ProdutoLidos>0){
         for(int c=0;c<ProdutoLidos;c++){
-            Serial.println(c+": "+estoqProd[c]);
-            Serial.println("  Valor: "+estoqValor[c]);
-            Serial.println("QUANTIDADE: "+cont[c]);
+            Serial.println((String)"\n"+(c+1)+": "+estoqProd[c]);
+            Serial.println((String)"  Valor: R$"+estoqValor[c]+",00");
+            Serial.println((String)"QUANTIDADE: "+cont[c]);
           }
       }
-      Serial.println("------------------------");
-        deku++;
-        }}
-    //----------------------------------------  
-  }
+      Serial.println("------------------------\n");
 
-}
+      tProd="";
+      tVal="";
+  } 
